e23940d009ca28d8ce141bfd949d395fd7eebbb3
dmrgn <danielrmorgan11@gmail.com>
2022-09-11 15:28:50 -0400

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
-
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx(), preact(), sitemap()],
+  integrations: [tailwind(), image(), mdx(), preact(), sitemap(), robotsTxt()],
   markdown: {
     shikiConfig: {
       theme: 'dark-plus'
     }
+  },
+  experimental: {
+    integrations: true,
   },
   site: 'https://readytoprogram.netlify.app'
 });
